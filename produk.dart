55fdce40d4df1f45b18211116452d46a8137190a
Kamilafp <150779636+Kamilafp@users.noreply.github.com>
2024-10-01 14:30:25 +0700

    Create produk.dart

---
+++ produk.dart
@@ -0,0 +1,23 @@
+class Produk {
+  int id;
+  String kodeProduk;
+  String namaProduk;
+  int hargaProduk;
+
+  Produk({
+    required this.id,
+    required this.kodeProduk,
+    required this.namaProduk,
+    required this.hargaProduk,
+  });
+
+  // If you want to create the Produk object from JSON
+  factory Produk.fromJson(Map<String, dynamic> obj) {
+    return Produk(
+      id: obj['id'],
+      kodeProduk: obj['kode_produk'],
+      namaProduk: obj['nama_produk'],
+      hargaProduk: obj['harga_produk'],
+    );
+  }
+}
