45a3191f79a39d140c3c26ded941caee092a0973
Kamilafp <150779636+Kamilafp@users.noreply.github.com>
2024-10-08 22:18:07 +0700

    Update registrasi.dart

--- registrasi.dart
+++ registrasi.dart
@@ -2,13 +2,9 @@ class Registrasi {
   int? code;
   bool? status;
   String? data;
-
   Registrasi({this.code, this.status, this.data});
-
   factory Registrasi.fromJson(Map<String, dynamic> obj) {
-  return Registrasi(
-  code: obj['code'],
-  status: obj['status'],
-  data: obj['data']);
+    return Registrasi(
+        code: obj['code'], status: obj['status'], data: obj['data']);
   }
-  }
+}
