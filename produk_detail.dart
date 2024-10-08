3ab3042cf7ee68e0350d4da78e3055a08b40e21c
Kamilafp <150779636+Kamilafp@users.noreply.github.com>
2024-10-08 22:18:56 +0700

    Update produk_detail.dart

--- produk_detail.dart
+++ produk_detail.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:tokokita/bloc/produk_bloc.dart';
 import 'package:tokokita/model/produk.dart';
 import 'package:tokokita/ui/produk_form.dart';
-
+import 'package:tokokita/ui/produk_page.dart';
+import 'package:tokokita/widget/warning_dialog.dart';
+// ignore: must_be_immutable
 class ProdukDetail extends StatefulWidget {
   Produk? produk;
 
@@ -14,72 +17,83 @@ class ProdukDetail extends StatefulWidget {
 class _ProdukDetailState extends State<ProdukDetail> {
   @override
   Widget build(BuildContext context) {
-  return Scaffold(
-  appBar: AppBar(
-  title: const Text('Detail Produk Kamila'),
-  ),
-  body: Center( child:
-  Column(
-  children: [
-  Text(
-  "Kode : ${widget.produk!.kodeProduk}",
-      style: const TextStyle(fontSize: 20.0),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Detail Produk Kamila'),
       ),
-      Text(
-          "Nama : ${widget.produk!.namaProduk}",
-          style: const TextStyle(fontSize: 18.0),
+      body: Center(
+        child: Column(
+          children: [
+            Text(
+              "Kode : ${widget.produk!.kodeProduk}",
+              style: const TextStyle(fontSize: 20.0),
+            ),
+            Text(
+              "Nama : ${widget.produk!.namaProduk}",
+              style: const TextStyle(fontSize: 18.0),
+            ),
+            Text(
+              "Harga : Rp. ${widget.produk!.hargaProduk.toString()}",
+              style: const TextStyle(fontSize: 18.0),
+            ),
+            _tombolHapusEdit()
+          ],
+        ),
       ),
-    Text(
-    "Harga : Rp. ${widget.produk!.hargaProduk.toString()}",
-    style: const TextStyle(fontSize: 18.0),
-    ),
-    _tombolHapusEdit()
-    ],
-    ),
-    ),
     );
-    }
+  }
 
   Widget _tombolHapusEdit() {
     return Row(
-    mainAxisSize: MainAxisSize.min,
-    children: [
-    //Tombol Edit
-    OutlinedButton(
-    child: const Text("EDIT"),
-    onPressed: () {
-    Navigator.push(
-    context,
-    MaterialPageRoute(
-    builder: (context) => ProdukForm(
-    produk: widget.produk!,
-    )));
-    }),
-    //Tombol Hapus
-    OutlinedButton(
-    child: const Text("DELETE"), onPressed: () => confirmHapus()),
-    ],
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        //Tombol Edit
+        OutlinedButton(
+            child: const Text("EDIT"),
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => ProdukForm(
+                        produk: widget.produk!,
+                      )));
+            }),
+        //Tombol Hapus
+        OutlinedButton(
+            child: const Text("DELETE"), onPressed: () => confirmHapus()),
+      ],
     );
-    }
+  }
 
   void confirmHapus() {
     AlertDialog alertDialog = AlertDialog(
-        content: const Text("Yakin ingin menghapus data ini?"),
-    actions: [
-    //tombol hapus
-    OutlinedButton(
-    child: const Text("Ya"),
-    onPressed: () {},
-    ),
-    //tombol batal
-    OutlinedButton(
-    child: const Text("Batal"),
-    onPressed: () => Navigator.pop(context),
-    )
-    ],
+      content: const Text("Yakin ingin menghapus data ini?"),
+      actions: [
+//tombol hapus
+        OutlinedButton(
+          child: const Text("Ya"),
+          onPressed: () {
+            ProdukBloc.deleteProduk(id: int.parse(widget.produk!.id!)).then(
+                    (value) => {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const ProdukPage()))
+                }, onError: (error) {
+              showDialog(
+                  context: context,
+                  builder: (BuildContext context) => const WarningDialog(
+                    description: "Hapus gagal, silahkan coba lagi",
+                  ));
+            });
+          },
+        ),
+//tombol batal
+        OutlinedButton(
+          child: const Text("Batal"),
+          onPressed: () => Navigator.pop(context),
+        )
+      ],
     );
-
     showDialog(builder: (context) => alertDialog, context: context);
-    }
   }
+}
 
